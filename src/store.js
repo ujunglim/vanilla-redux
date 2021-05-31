04c4bf26220f91fe2fa72d5b3304f4c35af151b5
Yuyu <ujunglim@naver.com>
2021-05-31 16:20:02 +0800

    add createReducer

--- src/store.js
+++ src/store.js
@@ -4,17 +4,6 @@ import {createAction, createReducer} from "@reduxjs/toolkit";
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.type: 
-//       return [{text: action.payload, id: Date.now()}, ...state]
-//     case deleteToDo.type:
-//       return state.filter(toDo => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// }
-
 const reducer = createReducer([], {
   [addToDo]: (state, action) => {
     state.unshift({text: action.payload, id: Date.now()})
